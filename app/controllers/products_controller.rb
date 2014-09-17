96cc87a1cb56fea99d3304033f172500ed53042e
Juliana Crispo <julianacrispo@gmail.com>
2014-09-17 15:33:39 -0400

    products index controller functions

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,21 @@
+class ProductsController < ApplicationController
+  
+  def index
+    @products = Product.all.order("created_at DESC") #todo: sort products by activity in descending order
+    if params[:search]
+      @products = Product.search(params[:search]).order("created_at DESC")
+    else
+     @products = Product.all.order('created_at DESC')
+   end
+  end
+
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   has_many :uses
+
+  def self.search(query)
+    where("name like ?", "%#{query}%") #todo: autopop and return not exact searches
+  end
 end
