efec65fcbb9c0503672173d0f97fbbb22df1ec85
Juliana Crispo <julianacrispo@gmail.com>
2014-09-19 16:18:42 -0400

    updated gitignore

--- app/controllers/uses_controller.rb
+++ app/controllers/uses_controller.rb
@@ -1,28 +1,33 @@
 class UsesController < ApplicationController
+  respond_to :html, :js
 
   def show
     @use = @product.uses
   end
 
-  def create
-    @use = @product.uses.build (use_params)
-    @use.product = @product
+def create
+  @product = Product.find(params[:product_id])
+  @uses = @product.uses
 
-    if @use.save
-      flash[:notice] = "Use case was saved"
-    else
-      flash[:error] = "There was an error saving your use case. Please try again"
-    end
+  @use = current_user.uses.build( use_params )
+  @use.product = @product
+  
 
-    redirect_to @product
+  if @use.save
+    flash[:notice] = "Your use case was created!"
+  else
+    flash[:error] = "There was an error saving your use case. Please try again."
   end
 
-  private
+  respond_with(@use) do |format|
+    format.html { redirect_to [@product]}
+  end
+end
+
+private
 
   def use_params
-    params.require(:use).permit(
-      :description
-      )
+    params.require(:use).permit(:description)
   end
 
 end
