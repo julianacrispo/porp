df502fe6effdfa1b2471b042c08ab6efb0373874
Juliana Crispo <julianacrispo@gmail.com>
2014-09-22 14:35:02 -0400

    style changes and model changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   has_many :uses
+  has_many :users
 
   def self.search(query)
     where("name like ?", "%#{query}%") #todo: autopop and return not exact searches
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :uses
+  has_many :products
 end
